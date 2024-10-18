cb40dc13698717d98652c78bc89efec0d51af03d
Troy Motte <tmotte@natera.com>
2024-10-18 17:37:10 -0500

    minor flaws with run.sh

--- example/run-cygwin.sh
+++ example/run-cygwin.sh
@@ -5,4 +5,4 @@ mvn --batch-mode clean install || exit 1
 # Running out of temp dir so I can do compiles while still running:
 mkdir -p temp
 cp target/hello*.jar ./temp/ || exit 1
-java -Xmx15m -jar ./temp/hello-1.0.0.jar "$@"
+java -Xmx15m -jar "$(cygpath -w ./temp/hello-1.0.0.jar)" "$@"
